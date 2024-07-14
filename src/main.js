a5d67850c23d4833e231de38d50aa471337731ef
AIHardCore <aihardcore@163.com>
2024-07-15 03:10:03 +0800

    引入vant2 toast轻提示和dialog弹窗提示

--- src/main.js
+++ src/main.js
@@ -27,10 +27,12 @@ import './assets/icons' // icon
 import './router/index' // permission control
 
 // 导入Vant2
-import Vant, { Lazyload } from 'vant'
+import Vant, { Lazyload, Toast, Dialog } from 'vant'
 import 'vant/lib/index.css'
 
 Vue.use(Vant)
+Vue.use(Toast)
+Vue.use(Dialog)
 Vue.use(Lazyload)
 Vue.use(checkPer)
 Vue.use(permission)
