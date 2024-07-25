1d5be88aea9174ee20d3962cdd6f0639bca38c68
AIHardCore <aihardcore@163.com>
2024-07-26 05:22:03 +0800

    修改授权完成跳转首页的方式(因为之前跳转太慢)

--- src/views/app/Auth.vue
+++ src/views/app/Auth.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="main" />
+  <div class="main">
+    <van-dialog v-model="show" title="登录中..." :show-cancel-button="false" :show-confirm-button="false" />
+  </div>
 </template>
 
 <script>
 import crudLogin from '@/api/app/login'
 import { setAppToken } from '@/utils/auth'
+import store from '@/store'
 
 export default {
   name: 'AuthPage',
+  data() {
+    return {
+      show: true
+    }
+  },
   created() {
     this.path = this.$route.name
     const index = window.location.href.indexOf('?')
@@ -21,7 +29,9 @@ export default {
     if (this.code) {
       this.login()
     } else {
-      location.href = window.location.protocol + '//' + window.location.host + '/#/Home'
+      this.$router.push({
+        path: '/'
+      })
     }
   },
   methods: {
@@ -31,7 +41,10 @@ export default {
       }
       crudLogin.login(data).then(res => {
         setAppToken(res.token, true)
-        location.href = window.location.protocol + '//' + window.location.host + '/#/Home'
+        store.dispatch('app/jump', false)
+        this.$router.push({
+          path: '/'
+        })
       }).catch(() => {})
     }
   }
@@ -43,5 +56,19 @@ export default {
   overflow: auto !important;
   min-height: 100vh; /* 设置最小高度为视口的100% */
   overflow-y: auto; /* 如果内容超出屏幕，可以滚动查看 */
+}
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.block {
+  text-align: center;
+  width: 200px;
+  height: 150px;
+  line-height: 50px;
+  background-color: #fff;
 }
 </style>
